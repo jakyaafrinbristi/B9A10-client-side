9292ae890ca789c214cd0bf128a5af616cf2df29
jakyaafrinbristi <jakyaafrinb@gmail.com>
2024-05-19 17:33:16 +0600

    slider added

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -24,7 +24,7 @@ export default function Carousel() {
         centeredSlides={true}
         loop={true}
         autoplay={{
-          delay: 5000,
+          delay: 2000,
           disableOnInteraction: false,
         }}
         pagination={{
