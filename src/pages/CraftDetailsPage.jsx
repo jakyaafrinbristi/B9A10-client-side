55bd22f75b98b3124a596bd0fd650a4c03d15db7
jakyaafrinbristi <jakyaafrinb@gmail.com>
2024-05-30 02:53:16 +0600

    light and dark theme apply

--- src/pages/CraftDetailsPage.jsx
+++ src/pages/CraftDetailsPage.jsx
@@ -3,15 +3,15 @@ import { useLoaderData } from "react-router-dom";
 
 const CraftDetailsPage = () => {
     const card =useLoaderData();
-    console.log(card)
+    // console.log(card)
     const {_id,name,email,item_name,subcategory_Name,price,rating,processing,image,description,customization,stock}= card || {};
     return (
         <div className="container px-8 py-10 mx-auto ">
        <div className="  flex-row lg:flex gap-9">
-       <div className="lg:w-[60%]">
+       <div className="lg:w-[50%]">
         <img  src={image} alt="" />
         </div>
-        <div className="lg:w-[40%] space-y-4 mt-20">
+        <div className="lg:w-[50%] space-y-4 mt-20">
             <p className="text-center text-2xl font-bold ">{subcategory_Name}</p>
             <hr />
             <p className="text-center text-2xl font-bold ">{item_name}</p>
